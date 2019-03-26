44add444efd44a71df1bef7dafe0e0163ef8e8b8
James Merrix <james@appoly.co.uk>
2019-03-26 11:35:58 +0000

    Concept of a HTTPclient

---
+++ src/HttpClient/PayRunHttpClient.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Appoly\Payrun\HttpClient;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Middleware;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Subscriber\Oauth\Oauth1;
+use GuzzleHttp\Exception\ClientException;
+
+class PayRunHttpClient {
+
+	public function __construct()
+	{	
+		$this->consumer_key = config(['consumer_key' => 'imagick']);
+		$this->consumer_secret = config(['consumer_secret' => 'imagick']);
+		$this->signature_method = 'HMAC-SHA1';
+		$this->api_url = "https://api.test.payrun.io/";
+	}
+
+	public function call()
+	{
+		$stack = HandlerStack::create();
+		$middleware = new Oauth1(
+			[
+				'consumer_key' => $this->consumer_key,
+				'consumer_secret' => $this->consumer_secret,
+				'token_secret' => '',
+				'signature_method'=> $this->signature_method
+			]
+		);
+			
+		$stack->push($middleware);
+			
+		$client = new Client([
+			'base_uri' => $this->api_url,
+			'handler' => $stack
+		]);
+
+		dd($client);
+	}
+
+}
--- src/Payrun.php
+++ src/Payrun.php
@@ -2,10 +2,18 @@
 
 namespace Appoly\Payrun;
 
+use Appoly\Payrun\HttpClient\PayRunHttpClient;
+
 class Payrun
 {
     public function test()
     {
     	return 'Hello World';
+    }
+
+    public function testCall()
+    {
+    	$client = new PayRunHttpClient();
+    	return $client->call();
     }
 }
